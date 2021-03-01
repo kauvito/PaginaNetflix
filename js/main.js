4ab0c5c3283ab3d981cdc7238e76109e9609d533
Kauan Vitorino <kautec@outlook.com.br>
2021-03-01 17:05:54 -0300

    Pagina Netflix, bootcamp da DIO

---
+++ js/main.js
@@ -0,0 +1,10 @@
+function assitir()
+{
+    window.open("https://www.netflix.com/br/title/70178217");
+}
+
+function informacoes()
+{
+    window.location.href = "https://pt.wikipedia.org/wiki/House_of_Cards_(s%C3%A9rie_americana)";
+}
+
